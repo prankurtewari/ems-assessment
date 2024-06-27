2df6e428c2edb59f462e503f4ecfa3a7bcd73cb0
prankur tewari <prankur@prankurs-MacBook-Air.local>
2024-06-27 20:32:32 +0530

    Authentication in place

--- src/main/java/com/ems/assessment/employee/controller/SecurityController.java
+++ src/main/java/com/ems/assessment/employee/controller/SecurityController.java
@@ -1,28 +1,27 @@
-//package com.ems.assessment.employee.controller;
-//
-//import org.springframework.beans.factory.annotation.Configurable;
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-//import org.springframework.security.web.SecurityFilterChain;
-//
-//@Configuration
-//@EnableWebSecurity
-//public class SecurityController {
-//    @Bean
-//    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-//
-//        http.authorizeRequests()
-////                .antMatchers("/").permitAll()
-////                .antMatchers("/addEmployee").permitAll()
-//                .antMatchers("/").authenticated()
-//                .and()
-//                .formLogin().permitAll();
-//        return http.build();
-//    }
-//
-//    //...
-//}
-//
+package com.ems.assessment.employee.controller;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractAuthenticationFilterConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityController {
+@Bean
+public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+    return httpSecurity
+            .csrf(AbstractHttpConfigurer::disable)
+            .authorizeHttpRequests(registry -> {
+                registry.antMatchers("/api/home").permitAll();
+                registry.antMatchers("/api/employeeDetails").authenticated();
+                registry.anyRequest().authenticated();
+            })
+            .formLogin(AbstractAuthenticationFilterConfigurer::permitAll)
+            .build();
+}
+}
+
